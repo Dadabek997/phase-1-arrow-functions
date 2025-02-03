c297ca084a33891a467b643feaa1d4c581d70e3f
Dadabek Rajabov <barajabov@gmail.com>
2025-02-03 12:59:54 -0500

    Adds index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+divide(2000, 100)
+
+const square = function(x) {
+    return x * x
+}
+
+square(2)
+
+const add = function(num1, num2) {
+    return num1 + num2
+}
+
+add(3, 4)
